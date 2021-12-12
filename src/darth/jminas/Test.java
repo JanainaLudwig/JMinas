ba22b4ca5a74294afafad788b606ebeae77f99d1
Janaina Ludwig <janainaludwig@gmail.com>
2021-12-12 10:27:27 -0300

    sample test

---
+++ src/darth/jminas/Test.java
@@ -0,0 +1,14 @@
+package darth.jminas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		Assert.assertTrue(true);
+	}
+
+}
